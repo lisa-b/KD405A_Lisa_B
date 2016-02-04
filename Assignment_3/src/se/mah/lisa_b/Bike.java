669650377d55ef516ef74a7a6b4ccd9ba0a6e4c2
Lisa <lisa@bolmsten.se>
2016-02-04 20:25:13 +0100

    Lägg till JavaDoc kommentarer

--- Assignment_3/src/se/mah/lisa_b/Bike.java
+++ Assignment_3/src/se/mah/lisa_b/Bike.java
@@ -2,36 +2,64 @@ package se.mah.lisa_b;
 
 /**
  * Bike class
- * @author lisa
+ * @author Lisa Bolmsten
  *
  */
 public class Bike {
 	private String _color;
 	private int _price;
 	private int _size;
-	
+
+	/**
+	 * Contructor for creating a new bike.
+	 * @param color
+	 * @param size
+	 */
 	public Bike(String color, int size){
 		setColor(color);
 		setSize(size);
 	}
+	
+	/**
+	 * Constructor for creating a new bike.
+	 * @param price
+	 * @param color
+	 * @param size
+	 */
 	public Bike(int price, String color, int size) {
 		setColor(color);
 		setSize(size);
 		setPrice(price);
 	}
 	
+	/**
+	 * Get color of the bike.
+	 * @return color
+	 */
 	public String getColor(){
 		return _color;
 	}
 	
+	/**
+	 * Get size of the bike.
+	 * @return size
+	 */
 	public int getSize(){
 		return _size;
 	}
 	
+	/**
+	 * Get price of the bike.
+	 * @return price
+	 */
 	public int getPrice(){
 		return _price;
 	}
 	
+	/**
+	 * Set price of the bike, limited by MIN_PRICE and MAX_PRICE constants.
+	 * @param price
+	 */
 	public void setPrice(int price){
 		if(price>=Constants.MIN_PRICE && price<=Constants.MAX_PRICE){
 			_price = price;		
@@ -40,6 +68,10 @@ public class Bike {
 		}	
 	}
 	
+	/**
+	 * Set size of the bike, limited by MIN_SIZE and MAX_SIZE constants.
+	 * @param size
+	 */
 	public void setSize(int size){
 		if(size>=Constants.MIN_SIZE && size<=Constants.MAX_SIZE){
 			_size = size;	
@@ -48,6 +80,10 @@ public class Bike {
 		}
 	}
 	
+	/**
+	 * Set color of the bike, limited by values in COLOR constant.
+	 * @param color
+	 */
 	public void setColor(String color){
 		for(int i = 0; i<Constants.COLOR.length; i++){
 			if (Constants.COLOR[i].equals(color)){
--- Assignment_3/src/se/mah/lisa_b/Constants.java
+++ Assignment_3/src/se/mah/lisa_b/Constants.java
@@ -1,10 +1,35 @@
 package se.mah.lisa_b;
 
+/**
+ * Contsants used in the program 
+ * @author Lisa Bolmsten
+ *
+ */
 public class Constants {
+	/**
+	 * Minimum size of bike.
+	 */
 	public static final int MIN_SIZE = 8;
+	
+	/**
+	 * Maximum size of bike.
+	 */
+	
 	public static final int MAX_SIZE = 28;
+	
+	/**
+	 * Minimum price of bike.
+	 */
 	public static final int MIN_PRICE = 0;
+	
+	/**
+	 * Maximum price of bike.
+	 */
 	public static final int MAX_PRICE = 30000;
+	
+	/**
+	 * Allowed colors for bike.
+	 */
 	public static final String[] COLOR = new String[] {
 		"red",
 		"blue",
