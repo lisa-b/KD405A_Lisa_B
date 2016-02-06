d73124bc5e57733e61cdd170e35c75c29e453d01
Lisa <lisa@bolmsten.se>
2016-02-06 08:58:51 +0100

    Lägg till lite JavaDoc

--- Assignment_3/src/se/mah/lisa_b/BikeStore.java
+++ Assignment_3/src/se/mah/lisa_b/BikeStore.java
@@ -2,10 +2,19 @@ package se.mah.lisa_b;
 
 import java.util.ArrayList;
 
+/**
+ * BikeStore class with an arraylist of bikes
+ * @author Lisa Bolmsten
+ *
+ */
 public class BikeStore {
 
 	private ArrayList<Bike> bikes = new ArrayList<Bike>();
 	
+	/**
+	 * Get all bikes currently in the store as a formatted string.
+	 * @return formatted string
+	 */
 	public String getAllBikes(){
 		String cyklar = new String();
 		for(Bike cykel: bikes){
@@ -13,6 +22,13 @@ public class BikeStore {
 		}
 		return cyklar;
 	}
+	
+	/**
+	 * Add a new bike to the store.
+	 * @param color
+	 * @param size
+	 * @param price
+	 */
 	public void addBike(String color, int size, int price){
 		bikes.add(new Bike(price, color, size));
 	}
